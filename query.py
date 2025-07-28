8f7cbc9650824c14768e318982e3dfdf03a78ee9
Sally Twu <sallytwu1999@gmail.com>
2025-07-28 17:17:49 +0800

    add llm and prompt_template

--- query.py
+++ query.py
@@ -10,9 +10,18 @@ import argparse
 import openai
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
 CHROMA_DB_PATH = "db/chroma_db"
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+PROMPT_TEMPLATE = """
+Question: {question}
+Answer the question based only on the following context:
+{context}
+---
+"""
 
 def main():
     # Create CLI
@@ -26,18 +35,27 @@ def main():
     db = Chroma(persist_directory=CHROMA_DB_PATH, embedding_function=embedding_function)
 
     # Search the DB
-    result = db.similarity_search(query_text, k=3)
-    print(f"Query: {query_text}")
-    print(f"Results:{result}")       
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
+        return
+    
+    context_text = "\n\n---\n\n".join([doc.metadata.get("content") for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    print(prompt)
+
+    # Create the LLM
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4")
+    response_text = llm.invoke(prompt)
+
+    sources = [
+        doc.metadata.get('law_number', '') + " " + doc.metadata.get('law_title', '')
+        for doc, _score in results
+    ]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
 
 
 if __name__ == "__main__":
  main()
-
-"""
-    llm = ChatOpenAI(temperature=0, model_name="gpt-4")
-    qa_chain = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=db.as_retriever(search_kwargs={"k": 3}),
-    )"""
